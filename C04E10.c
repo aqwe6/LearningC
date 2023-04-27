5bba4b5895b49d7623ac755f957bf101f860162a
aqwe6 <131225172+aqwe6@users.noreply.github.com>
2023-04-27 01:04:40 -0300

    Chapter

---
+++ C04E10.c
@@ -0,0 +1,24 @@
+//Make a program that reads three positive integers and calculates one of the following averages according to a numeric value entered by the user and shown in the following table:
+//Number typed          Media
+//       1            Geometric
+//       2             Weighted
+//       3            Harmonica
+//       4            Arithmetic
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    double x,y,z,w;
+    printf("Numbers:");
+    scanf("%lf%lf%lf",&x,&y,&z);
+    printf("Number typed:");
+    scanf("%lf",&w);
+    if(w==1) printf("%lf",x*y*z);
+    else if(w==2) printf("%lf",(x+2*y+3*z)/6);
+    else if(w==3) printf("%lf",1/(1/x+1/y+1/z));
+    else printf("%lf",(x+y+z)/3);
+
+    system("pause");
+    return 0;
+}
