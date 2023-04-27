ca0fb9d76c07c919feedd98da31c5f8d0c95ae26
aqwe6 <131225172+aqwe6@users.noreply.github.com>
2023-04-27 01:24:47 -0300

    Create C04E11.c

---
+++ C04E11.c
@@ -0,0 +1,52 @@
+//Make a program that informs the month according to the number entered by the user. Example: Input = 4. Output = April.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    switch(x)
+    {
+    case 1:
+        printf("January\n");
+        break;
+    case 2:
+        printf("February\n");
+        break;
+    case 3:
+        printf("March\n");
+        break;
+    case 4:
+        printf("April\n");
+        break;
+    case 5:
+        printf("May\n");
+        break;
+    case 6:
+        printf("June\n");
+        break;
+    case 7:
+        printf("July\n");
+        break;
+    case 8:
+        printf("Agost\n");
+        break;
+    case 9:
+        printf("September\n");
+        break;
+    case 10:
+        printf("October\n");
+        break;
+    case 11:
+        printf("November\n");
+        break;
+    default :
+        printf("December\n");
+        break;
+    }
+
+    system("pause");
+    return 0;
+}
