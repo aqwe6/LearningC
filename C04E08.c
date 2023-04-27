432277d827647fcaf980c1217f12c657ba2908d6
aqwe6 <131225172+aqwe6@users.noreply.github.com>
2023-04-27 00:06:45 -0300

    Chapter

---
+++ C04E08.c
@@ -0,0 +1,23 @@
+//Write a program that, given a swimmer's age, classifies him or her in one of the following categories:Age Category
+//Infant A 5-7
+//Infant B 8-10
+//Juvenile A 11-13
+//Juvenile B 14-17
+//Senior over 18 years old
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int d;
+    printf("Age:");
+    scanf("%d",&d);
+    if(d>4 && d<8) printf("Infant A\n");
+    if(d>7 && d<11) printf("Infant B\n");
+    if(d>10 && d<14) printf("Juvenil A\n");
+    if(d>13 && d<18) printf("Juvenil B\n");
+    if(d>18) printf("Senior B");
+
+    system("pause");
+    return 0;
+}
