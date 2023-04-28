1bdebc38f2ec40b03f56cb494b706ec06c5dd73a
aqwe6 <131225172+aqwe6@users.noreply.github.com>
2023-04-28 04:22:59 -0300

    Create C06E21.c

---
+++ C06E21.c
@@ -0,0 +1,24 @@
+// Read an array of size 3 × 3. Then print the sum of the values contained in its main diagonal.
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    double X[3][3];
+    int y = 0;
+    for(int i = 0; i <= 2 ; i++)
+    {
+        for(int j = 0 ; j <=2 ; j++)
+        {
+            scanf("%lf",&X[i][j]);
+            if(i==j) y = y + X[i][j];
+
+        }
+    }
+    printf("%d\n",y);
+
+
+    system("Pause");
+    return 0;
+}
