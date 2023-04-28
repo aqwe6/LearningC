d61e1d7f881dc007995874fe2cd9bb156916c565
aqwe6 <131225172+aqwe6@users.noreply.github.com>
2023-04-28 05:48:27 -0300

    Create C06E26.c

---
+++ C06E26.c
@@ -0,0 +1,26 @@
+// Make a program that reads an array of size 5 × 5. Calculate and print the sum of the elements of this matrix that are above the main diagonal.
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    double X[5][5],Y[1][10];
+    int x=0;
+    for(int i = 0; i <= 4 ; i++)
+    {
+        for(int j = 0 ; j <=4 ; j++)
+        {
+            scanf("%lf",&X[i][j]);
+            if(i == j) x = x + X[i][j];
+
+        }
+
+    }
+
+    printf("%d\n",x);
+
+
+    system("Pause");
+    return 0;
+}
