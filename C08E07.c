eb0f65db786283bc846edc0eab4d51cb75e3113b
aqwe6 <131225172+aqwe6@users.noreply.github.com>
2023-05-01 05:24:34 -0300

    Update C08E07.c

--- C08E07.c
+++ C08E07.c
@@ -16,7 +16,7 @@ int main()
     for(int i = 0 ; i < 5 ; i++)
     {
         scanf("%d%d%d",&x[i].hour,&x[i].minute,&x[i].second);
-        if(x[i].hour>a)
+        if(x[i].hour > a)
         {
             a = x[i].hour;
             b = x[i].minute;
@@ -24,14 +24,14 @@ int main()
         }
         else if(x[i].hour == a)
         {
-            if(x[i].minute>b)
+            if(x[i].minute > b)
             {
                 b = x[i].minute;
                 c = x[i].second;
             }
             else if(x[i].minute == b)
             {
-                c = x[i].second;
+                if(x[i].second > c) c = x[i].second;
             }
         }
     }
