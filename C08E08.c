5456cdf7778086910b6e092cb95c9e952ddb471e
aqwe6 <131225172+aqwe6@users.noreply.github.com>
2023-05-01 06:10:20 -0300

    Update C08E08.c

--- C08E08.c
+++ C08E08.c
@@ -14,6 +14,7 @@ int main()
 {
     struct H x[6];
     int a=0,b=0,c=0,y=0;
+    int a1=0,b1=0,c1=0,z=0;
     for(int i = 0 ; i < 6 ; i++)
     {
         gets(x[i].name);
@@ -49,7 +50,33 @@ int main()
 
             }
         }
+        if(x[i].year > a1)
+        {
+            a1 = x[i].year;
+            b1 = x[i].month;
+            c1 = x[i].day;
+            z = i;
+        }
+        else if(x[i].year == a1)
+        {
+            if(x[i].month > b1)
+            {
+                b1 = x[i].month;
+                c1 = x[i].day;
+                z = i;
+            }
+            else if(x[i].month == b1)
+            {
+                if(x[i].day > c1)
+                {
+                    c1 = x[i].day;
+                    z = i;
+                }
+
+            }
+        }
     }
+    printf("%s %dD/%dM/%dY\n",x[z].name,x[z].day,x[z].month,x[z].year);
     printf("%s %dD/%dM/%dY\n",x[y].name,x[y].day,x[y].month,x[y].year);
 
 
