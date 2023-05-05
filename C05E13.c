675f59dc16e6384defee724a64d479e01e6db10e
aqwe6 <131225172+aqwe6@users.noreply.github.com>
2023-05-05 06:03:45 -0300

    Update C05E13.c

--- C05E13.c
+++ C05E13.c
@@ -1,6 +1,6 @@
 //Make a program that displays the sum of all natural numbers below 1,000 that are multiples of 3 or 5.
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> 
 
 int main()
 {
