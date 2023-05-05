986e9b8ee8d5eee074291fa4c3d428dfb4faeba2
aqwe6 <131225172+aqwe6@users.noreply.github.com>
2023-05-05 05:18:10 -0300

    Create C09E19.c

---
+++ C09E19.c
@@ -0,0 +1,30 @@
+// Make a function that takes as a parameter the value of an angle in degrees and calculate the cosine value of this angle using its respective Taylor series:
+// sen x = x - x^2/2! + x^4/4!...
+// where x is the value of the angle in radians. Consider π = 3.1414592 and n varying from 0 to 5.
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+double k (double x)
+{
+    double d = x,e = 1;
+    for(double i = 1 ; i <= 5 ; i++)
+    {
+        e = e*x*x/(2*i*(2*i-1))*(-1);
+        d = d + e;
+    }
+
+    return d;
+}
+
+int main()
+{
+
+    double a;
+    scanf("%lf",&a);
+    printf("%lf\n",k(a));
+
+    system("pause");
+    return 0;
+}
