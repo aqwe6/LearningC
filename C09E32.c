8a8762fc6d566d5752b49cc0bdcdf072f83c8984
aqwe6 <131225172+aqwe6@users.noreply.github.com>
2023-05-06 11:06:23 -0300

    Update C09E32.c

--- C09E32.c
+++ C09E32.c
@@ -3,14 +3,14 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int k(int *X[5])
+int k(int X[5])
 {
     int b = 0;
     int c = 0;
     for(int f=0 ; f < 5 ; f++)
     {
-        if(*X[f]%2 == 0) b = b+*X[f];
-        else c = c+*X[f];
+        if(X[f]%2 == 0) b = b+X[f];
+        else c = c+X[f];
     }
     return printf("%d\n%d\n",b,c);
 }
@@ -19,7 +19,7 @@ int main()
 {
     int A[5];
     for(int i = 0 ; i<5 ; i++) scanf("%d",&A[i]);
-    k(&A);
+    k(A);
 
     system("pause");
     return 0;
