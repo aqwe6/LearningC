ca1e55855f3d6e4ba72429e672c303f857716ffa
aqwe6 <131225172+aqwe6@users.noreply.github.com>
2023-05-08 10:41:48 -0300

    Create C11E09.c

---
+++ C11E09.c
@@ -0,0 +1,30 @@
+// Create a function that receives a string and returns the pointer to that inverted string.
+
+#include<stdio.h>
+#include<stdlib.h>
+
+char X(char N[50])
+{
+    int k=0;
+    int d,e;
+    for(int i=0;N[i]!='\0';i++) k++;
+    k = k-1;
+    for(int i=0;i<=(k/2);i++)
+    {
+        d = N[i];
+        e = N[k-i];
+        N[k-i] = d;
+        N[i] = e;
+    }
+    return printf("%s\n",N);
+}
+
+int main()
+{
+    char l[50];
+    gets(l);
+    X(l);
+
+    system("Pause");
+    return 0;
+}
