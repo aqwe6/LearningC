bb6830b8b4e606b4973cfedae5c4082855758db0
aqwe6 <131225172+aqwe6@users.noreply.github.com>
2023-05-08 10:57:02 -0300

    Create C11E10.c

---
+++ C11E10.c
@@ -0,0 +1,25 @@
+// Write a function that takes a positive integer value N per parameter and returns the pointer to a dynamically allocated vector of size N.
+// This vector should have its elements filled in with a certain value, also passed by parameter. 
+// If N is negative or equal to zero, a null pointer must be returned.
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int X(int N)
+{
+    int *p;
+    for(int i=0;i<N;i++) p[i] = i;
+    p = (int *) malloc(N*sizeof(int));
+    if(N<1) *p = NULL;
+    return printf("%d\n",*p);
+}
+
+int main()
+{
+    int l;
+    scanf("%d",&l);
+    X(l);
+
+    system("Pause");
+    return 0;
+}
